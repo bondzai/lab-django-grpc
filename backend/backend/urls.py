6a27c75dc31908d01e80fcf1bf9d2f4163dec487
JamesBond <introbond.dev@gmail.com>
2024-03-23 10:19:21 +0700

    Add gRPC handlers for blog service

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.handlers import grpc_handlers as blog_grpc_handlers
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+def grpc_handlers(server):
+    blog_grpc_handlers(server)
