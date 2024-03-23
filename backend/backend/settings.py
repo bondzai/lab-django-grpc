1f7c81d9e920abfbaaf7bb6d5ca74b66e6387851
JamesBond <introbond.dev@gmail.com>
2024-03-23 10:05:49 +0700

    Add django_grpc_framework and blog to INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_grpc_framework',
+    'blog',
 ]
 
 MIDDLEWARE = [
